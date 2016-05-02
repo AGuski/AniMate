2e97a8e562b405db3115add6e5672f440979b53f
Alexander Guski <alex@weareomega.net>
2016-05-02 16:05:40 +0200

    alex/demo-extension/AN-4

---
+++ alex_demo_extension/content.js
@@ -0,0 +1,15 @@
+// content.js
+console.log("Content Script loading...");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+    	console.log("Blah");
+	}
+  }
+);
+
+// Do Stuff with inspectedElement from devtools.js
+function doSelectedElement(el) {
+    console.log(el);
+}
---
+++ alex_demo_extension/panel.js
@@ -0,0 +1,19 @@
+/* Establishes the connection to the background.js */
+var bgPageConnection = chrome.runtime.connect({
+    name: "panel"
+});
+
+
+bgPageConnection.postMessage({
+    content: "Message from Panel!"
+});
+
+/* Called when message from the Background Page is received */
+bgPageConnection.onMessage.addListener(function(message) {
+
+	window.document.body.appendChild(document.createTextNode(message.content));
+
+	/* Example: return the message to background.js */
+	//bgPageConnection.postMessage({content: message});
+
+});
