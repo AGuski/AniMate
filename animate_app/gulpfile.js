1420e3b29c602249b20b75bf6ebc97f6f4d740c5
Alexander Guski <alex@weareomega.net>
2016-05-23 20:36:13 +0200

    refactor/switch-extension-to-app/AN-44

--- animate_app/gulpfile.js
+++ animate_app/gulpfile.js
@@ -1,70 +1,137 @@
 'use strict';
 
-//npm install gulp gulp-minify-css gulp-uglify gulp-clean gulp-cleanhtml gulp-jshint gulp-strip-debug gulp-zip --save-dev
-
-var gulp = require('gulp'),
-    // clean = require('gulp-clean'),
-    // cleanhtml = require('gulp-cleanhtml'),
+var gulp       = require('gulp'),
+    gutil      = require('gulp-util'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify     = require('gulp-uglify'),
+    htmlmin    = require('gulp-htmlmin'),
+    replace    = require('gulp-replace'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    es         = require('event-stream'),
+    browserify = require('browserify'),
+    babelify   = require('babelify'),
+    source     = require('vinyl-source-stream'),
+    buffer     = require('vinyl-buffer'),
+    eslint     = require('gulp-eslint'),
+    templateCache = require('gulp-angular-templatecache'),
+    sass       = require('gulp-sass'),
+    concat  = require('gulp-concat'),
+    clean      = require('gulp-clean'),
     // minifycss = require('gulp-minify-css'),
-    // jshint = require('gulp-jshint'),
     // stripdebug = require('gulp-strip-debug'),
-    // uglify = require('gulp-uglify'),
     // zip = require('gulp-zip');
-    tinylr = require('tiny-lr');
+    tinylr     = require('tiny-lr');
 
-gulp.task('dev', function () {
-    var lr = tinylr();
-    lr.listen(35729);
-    gulp.watch(['src/**.{js,css,html}'], function (evt) {
-        lr.changed({
-            body: {
-                files: [evt.path]
-            }
-        });
-    });
+// Target directory for build and live watch
+
+var src = './app'
+var build = './dist'
+
+var paths = {
+  scss: ['app/**/*.scss']
+}
+
+gulp.task('lint', function () {
+  return gulp.src([src+'/**/*.js'])
+  .pipe(eslint())
+  // eslint.format() outputs the lint results to the console.
+  // Alternatively use eslint.formatEach() (see Docs).
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
 });
 
-// //clean build directory
-// gulp.task('clean', function() {
-//     return gulp.src('dist/*', {read: false})
-//         .pipe(clean());
-// });
+//copy the manifest to build
+gulp.task('manifest', function() {
+  return gulp.src(src+'/manifest.json')
+  .pipe(gulp.dest(build));
+});
 
-// //copy static folders to build directory
-// gulp.task('copy', function() {
-//     gulp.src('src/fonts/**')
-//         .pipe(gulp.dest('dist/fonts'));
-//     gulp.src('src/icons/**')
-//         .pipe(gulp.dest('dist/icons'));
-//     gulp.src('src/_locales/**')
-//         .pipe(gulp.dest('dist/_locales'));
-//     return gulp.src('src/manifest.json')
-//         .pipe(gulp.dest('dist'));
-// });
+// scripts
+gulp.task('scripts', ['lint'], function() {
 
-// //copy and compress HTML files
-// gulp.task('html', function() {
-//     return gulp.src('src/*.html')
-//         .pipe(cleanhtml())
-//         .pipe(gulp.dest('dist'));
-// });
+  // Main files must be present for extension
+  var files = ['background.js',
+               'app.js'
+              ];
 
-// //run scripts through JSHint
-// gulp.task('jshint', function() {
-//     return gulp.src('src/**.js')
-//         .pipe(jshint())
-//         .pipe(jshint.reporter('default'));
-// });
+  var tasks = files.map(function(entry){
+    return browserify({
+      // entries: entry file
+      entries: src+'/'+entry,
+      debug: true
+    })
+    // Use Babel with es2015
+    .transform(babelify.configure({
+      presets: ["es2015"]
+    }))
+    .bundle()
+    // source: output file
+    .pipe(source('./'+entry))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      // Add transformation tasks to the pipeline here.
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(build));
 
-// //copy vendor scripts and uglify all other scripts, creating source maps - NOT DONE
-// gulp.task('scripts', ['jshint'], function() {
-//     gulp.src('src/scripts/vendors/**/*.js')
-//         .pipe(gulp.dest('dist/scripts/vendors'));
-//     return gulp.src(['src/scripts/**/*.js', '!src/scripts/vendors/**/*.js'])
-//         .pipe(stripdebug())
-//         .pipe(uglify({outSourceMap: true}))
-//         .pipe(gulp.dest('dist/scripts'));
-// });
+  });
+  return es.merge.apply(null, tasks);
+});
+
+// html
+gulp.task('html', function() {
+  gulp.src(src+'/modules/**/*.html')
+  .pipe(templateCache('templateCacheHtml.js', {
+      standalone: true,
+      module: 'templates',
+      root: 'modules'
+    }))
+    .pipe(gulp.dest(build));
+
+  return gulp.src(src+'/app.html')
+  .pipe(replace('../bower_components', 'vendor'))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest(build));
+});
+
+gulp.task('styles', function(){
+  return gulp.src(paths.scss)
+  .pipe(sourcemaps.init())
+  .pipe(concat('styles.scss'))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(build));
+});
+
+gulp.task('static', function(){
+  return gulp.src(src+'/assets/**/*')
+  .pipe(gulp.dest(build+'/assets'));
+});
+
+gulp.task('vendor', function(){
+  return gulp.src('./bower_components/**/*')
+  .pipe(gulp.dest(build+'/vendor'));
+});
+
+gulp.task('dev', ['manifest', 'scripts', 'html', 'styles', 'static', 'vendor'], function () {
+  var lr = tinylr();
+  lr.listen(35729);
+  gulp.watch([src+'/**/*.{js,css,scss,html,json}'], ['manifest', 'scripts', 'html', 'styles', 'static', 'vendor'], function (evt) {
+      lr.changed({
+          body: {
+              files: [evt.path]
+          }
+      });
+  });
+});
+
+//clean build directory
+gulp.task('clean', function() {
+    return gulp.src(build+'/*', {read: false})
+        .pipe(clean());
+});
 
 // //minify styles
 // gulp.task('styles', function() {
@@ -77,7 +144,7 @@ gulp.task('dev', function () {
 
 // //build ditributable and sourcemaps after other tasks completed
 // gulp.task('zip', ['html', 'scripts', 'styles', 'copy'], function() {
-//     var manifest = require('./src/manifest'),
+//     var manifest = require(src+'/manifest'),
 //         distFileName = manifest.name + ' v' + manifest.version + '.zip',
 //         mapFileName = manifest.name + ' v' + manifest.version + '-maps.zip';
 //     //collect all source maps
@@ -93,4 +160,8 @@ gulp.task('dev', function () {
 // //run all tasks after build directory has been cleaned
 // gulp.task('default', ['clean'], function() {
 //     gulp.start('zip');
+// });
+// 
+// gulp.task('default', ['lint'], function () {
+//     // This will only run if the lint task is successful...
 // });
