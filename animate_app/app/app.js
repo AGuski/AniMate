3832979aec2e33ac1dc7ada7374b4f858152ab8f
Alexander Guski <alex@weareomega.net>
2016-05-31 02:34:58 +0200

    feat/injectable-webview-component/AN-49

--- animate_app/app/app.js
+++ animate_app/app/app.js
@@ -1,17 +1,19 @@
 "use strict";
 
-console.log('App init...');
-
 import { runBlock } from './modules/index.run';
 import { stateConfig} from './modules/index.states';
 
 /* Components */
 import appBarComponent from './modules/gui/components/app-bar-component/app-bar.component';
+import anWebviewComponent from './modules/an-webview/an-webview.component';
 
+/* Directives */
+import { CompileDirective } from './modules/core/directives/compile/compile.directive';
 
 angular.module('aniMateApp', [
     'ui.router',
     'ngAnimate',
+    'ngSanitize',
     'templates'
   ])
 
@@ -20,6 +22,16 @@ angular.module('aniMateApp', [
 
   /* Components */
   .component('appBar', appBarComponent)
+  .component('anWebview', anWebviewComponent)
+
+  /* Directives */
+  .directive('compile', CompileDirective)
+
+  .config(function($sceProvider) {
+    // Completely disable SCE.  For demonstration purposes only!
+    // Do not use in new projects.
+    $sceProvider.enabled(false);
+  })
 
   .run(['$state', function ($state) {
 		$state.go('main');
