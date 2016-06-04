9f69134e5e1a4b849e6fda5a758de0a366fcf05d
Ammon <ammon-mino@web.de>
2016-06-04 23:47:25 +0200

    feat/Propertylist-added/AN-46

---
+++ animate_app/app/modules/gui/components/timeline/timeline.component.js
@@ -0,0 +1,12 @@
+let TimelineController = class {
+	constructor() {
+		'ngInject';
+	}
+}
+
+export default {
+	templateUrl: 'modules/gui/components/timeline/timeline.html',
+	controller: TimelineController,
+	controllerAs: 'ctrl',
+	bindings:{}
+}
